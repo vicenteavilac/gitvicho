671845cf028b237fba88cfbc0ac0c96f77af8a44
vicenteavilac <166768570+vicenteavilac@users.noreply.github.com>
2024-04-18 20:05:19 -0400

    Add files via upload

---
+++ Perimetro.py
@@ -0,0 +1,5 @@
+def perimetro_cuadrado(lado):
+    return 4 * lado
+
+lado = float(input("Ingrese la longitud del lado del cuadrado: "))
+print("El perímetro del cuadrado es:", perimetro_cuadrado(lado))
